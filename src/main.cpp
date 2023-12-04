443bb4e020189d88ad0bbceb3610d48b12141eac
Jaws370 <jackawerner+git@gmail.com>
2023-12-04 14:47:10 -0800

    project scaffolding in place, ready for coding

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <SFML/Graphics.hpp>
+#include "rendering/rendering.h"
+
+int main()
+{
+    
+    Rendering hi("awesomechess");
+
+    return 0;
+}
---
+++ src/rendering/rendering.h
@@ -0,0 +1,13 @@
+#ifndef RENDERING_H
+#define RENDERING_H
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Rendering
+{
+public:
+    Rendering(std::string windowName);
+};
+
+#endif
