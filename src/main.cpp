828b4f05779a21ae63e2a27528fbae98d22196e9
Jaws370 <jackawerner+git@gmail.com>
2023-12-05 22:00:37 -0800

    v0.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <SFML/Graphics.hpp>
-#include "rendering/rendering.h"
 
 int main()
 {
-    
-    Rendering hi("awesomechess");
+
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "awesomechess");
+
+    // Run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // Check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear the window with a black color
+        window.clear(sf::Color::Black);
+
+       // Display the window contents on screen
+       window.display();
+       
+   }
 
     return 0;
 }
