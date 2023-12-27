1cebb6e8a478182efdd664c59a2f8ed8065585a6
Jack Werner <jackawerner+git@gmail.com>
2023-12-27 11:29:10 -0800

    formatting changes

--- src/main.cpp
+++ src/main.cpp
@@ -11,68 +11,71 @@ int main()
 {
 
   Board board;
-  sf::RectangleShape(*boards)[8] = board.getRectangle();
-
-  Pieces pieces;
-  Piece(*p)[8] = pieces.getPieces();
+  sf::RectangleShape(*boards)[8]{board.getRectangle()};
+  ~Pieces pieces;
+  Piece(*p)[8]{pieces.getPieces()};
 
   sf::RenderWindow window(sf::VideoMode(512, 512), "AwesomeChess");
 
-  sf::Vector2i position;
-  // Run the program as long as the window is open
+  // run the program as long as the window is open
   while (window.isOpen())
   {
-    // Check all the window's events that were triggered since the last iteration of the loop
+    // check all the window's events that were triggered since the last iteration of the loop
     sf::Event event;
     while (window.pollEvent(event))
     {
+      // check if mouse buttons have been pressed
       if (event.type == sf::Event::MouseButtonPressed)
       {
         sf::Vector2i position = sf::Mouse::getPosition(window);
         int column{};
         int row{};
 
-        //saves the column that the mouse clicks too a variable
-        for (int i{1};i<=8;i++){
-          if(position.x<=64*i){
-            column=i;
+        // saves the column that the mouse clicks to a variable column
+        for (int i{1}; i <= 8; i++)
+        {
+          if (position.x <= 64 * i)
+          {
+            column = i;
           }
         }
 
-        //saves the row that the mouse clicks too a variable
-        for (int i{1};i<=8;i++){
-          if(position.y<=64*i){
-            row=i;
+        // saves the row that the mouse clicks to a variable row
+        for (int i{1}; i <= 8; i++)
+        {
+          if (position.y <= 64 * i)
+          {
+            row = i;
           }
         }
       }
-        
+
       // "close requested" event: we close the window
       if (event.type == sf::Event::Closed)
         window.close();
       // need to add code to change board in case of resizing and change position of pieces
     }
 
-    // Clear the window with a black color
+    // clear the window with a black color
     window.clear(sf::Color::Black);
 
-    for (int i = 0; i < 8; i++)
+    for (int i{0}; i < 8; i++)
     {
-      for (int j = 0; j < 8; j++)
+      for (int j{0}; j < 8; j++)
       {
         window.draw(boards[i][j]);
       }
     }
 
-    for (int i = 0; i < 8; i++)
+    for (int i{0}; i < 8; i++)
     {
-      for (int j = 0; j < 8; j++)
+      for (int j{0}; j < 8; j++)
       {
         window.draw(p[i][j].getSprite());
       }
     }
 
-    // Display the window contents on screen
+    // display the window contents on screen
     window.display();
   }
 
