b9a555e6fa3b9bd2789e072eb51b117fc66d7ace
tmeanstacocat <76409091+tmeanstacocat@users.noreply.github.com>
2024-01-26 08:54:59 -0800

    added the display class

---
+++ src/display/Display.hpp
@@ -0,0 +1,9 @@
+class displayBoard
+{
+    private:
+
+    public:
+    void changeSquare(std::vector<int> lastMoves);
+    void circleAroundPiece(std::vector<int> posMoves);
+    void addCircles(std::vector<int> posMoves);
+}
