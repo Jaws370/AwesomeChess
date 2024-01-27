731de0155105b595947fe0b0bc8097074cdfdfa8
Jack Werner <jackawerner+github@gmail.com>
2024-01-27 15:58:27 -0800

    added .clang-format file
    also fixed some weird errors

--- src/main.cpp
+++ src/main.cpp
@@ -2,100 +2,100 @@
 #include "./piece/Piece.hpp"
 #include "./pieces/Pieces.hpp"
 #include "./user_input/UserInput.hpp"
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <memory>
 
 /*
-
 TODO List:
-	PIECE MOVEMENT:
-	need to make it so you have to move king if under check
-	add computer to play against
+    PIECE MOVEMENT:
+    need to make it so you have to move king if under check
+    add computer to play against
 
-	AUDIO: (can be done in pieces.cpp, but need to import audio from sfml)
-	add sounds for actions such as pieces moving, king being in check, etc.
+    AUDIO: (can be done in pieces.cpp, but need to import audio from sfml)
+    add sounds for actions such as pieces moving, king being in check, etc.
 
-	DISPLAY: (need new classes fo sho)
-	NEED BETTER SPRITES FOR PIECES: HIGHER QUALITY (need to make sure that the pieces are not distorted)
-	add a main menu screen with a start button, and a screen that plays after someone wins, which displays who won and who lost, as well as a replay button
-	add a counter that counts the material lost and gained for each player, as well as an icon of the pieces lost, just like chess.com
-	make the pieces move instead of teleport
-	add a hud for player 1 and player 2
-	add dots to each square where the piece can move and for the king, make the dots red where he can't go
-	add a circle around pieces that could be captured when a piece is selected
-	letters and numbers on the edge of the board, labeled 1-8 on the left column and a-h on the bottom row
-	when a piece moves to a new square, make the old position and new position a lighter green if it is on a green square, and slightly yellow if on a white square
+    DISPLAY: (need new classes fo sho)
+    NEED BETTER SPRITES FOR PIECES: HIGHER QUALITY (need to make sure that the pieces are not distorted)
+    add a main menu screen with a start button, and a screen that plays after someone wins, which displays who won and who lost, as well as a replay button
+    add a counter that counts the material lost and gained for each player, as well as an icon of the pieces lost, just like chess.com
+    make the pieces move instead of teleport
+    add a hud for player 1 and player 2
+    add dots to each square where the piece can move and for the king, make the dots red where he can't go
+    add a circle around pieces that could be captured when a piece is selected
+    letters and numbers on the edge of the board, labeled 1-8 on the left column and a-h on the bottom row
+    when a piece moves to a new square, make the old position and new position a lighter green if it is on a green square, and slightly yellow if on a white square
 
-	USER INPUT:
-	allow for the chess pieces to be dragged and point-and-click
+    USER INPUT:
+    allow for the chess pieces to be dragged and point-and-click
 
-	CODE:
-	optimize functions to be smaller and faster while still being very understandable <- change getPossibleMoves to be more functional programming (higher order functions?)
-	needs to be better optimized memory wise
+    CODE:
+    optimize functions to be smaller and faster while still being very understandable <- change getPossibleMoves to be more functional programming (higher order functions?)
+    needs to be better optimized memory wise
 
-	DONE STUFF:
-	checking and movement for the king
-	added user input class
-	added board and pieces to heap to limit stack size
-	king now recognizes castling
-	en pissant
-	rook and pawn movement works for castling and en passant
-	window can now resize
-	changed the function getPossibleMoves to have multiple functions of get(&piece)Moves for easier replacement
-	need to add something to make sure king does not castle while under check
-	bugfix promoting to queen, rook, bishop, knight
+    DONE STUFF:
+    checking and movement for the king
+    added user input class
+    added board and pieces to heap to limit stack size
+    king now recognizes castling
+    en pissant
+    rook and pawn movement works for castling and en passant
+    window can now resize
+    changed the function getPossibleMoves to have multiple functions of get(&piece)Moves for easier replacement
+    need to add something to make sure king does not castle while under check
+    bugfix promoting to queen, rook, bishop, knight
 */
 
 int main()
 {
-	// unique pointers for board and pieces (allocates memory on the heap)
-	std::unique_ptr board = std::make_unique<Board>();
-	std::unique_ptr pieces = std::make_unique<Pieces>();
+    // unique pointers for board and pieces (allocates memory on the heap)
+    std::unique_ptr board  = std::make_unique<Board>();
+    std::unique_ptr pieces = std::make_unique<Pieces>();
 
-	UserInput in;
+    UserInput in;
 
-	sf::RenderWindow window(sf::VideoMode(512, 512), "AwesomeChess");
+    sf::RenderWindow window(sf::VideoMode(512, 512), "AwesomeChess");
 
-	// run the program as long as the window is open
-	while (window.isOpen())
-	{
-		// check all the window's events that were triggered since the last iteration of the loop
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
 
-			switch (event.type)
-			{
-			case sf::Event::MouseButtonPressed:
-				if (event.mouseButton.button == sf::Mouse::Left)
-				{
-					// get the position of the mouse and send it to be handled
-					in.handleLeftClick(sf::Mouse::getPosition(window), *pieces);
-				}
-				break;
-			case sf::Event::Closed:
-				window.close();
-				break;
-			case sf::Event::Resized:
-				window.setView(sf::View(visibleArea));
-				in.handleWindowResize(window, *board, *pieces);
-				break;
-			default:
-				break;
-			}
-		}
+            switch (event.type)
+            {
+                case sf::Event::MouseButtonPressed:
+                    if (event.mouseButton.button == sf::Mouse::Left)
+                    {
+                        // get the position of the mouse and send it to be handled
+                        in.handleLeftClick(sf::Mouse::getPosition(window), *pieces);
+                    }
+                    break;
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::Resized:
+                    window.setView(sf::View(visibleArea));
+                    in.handleWindowResize(window, *board, *pieces);
+                    break;
+                default:
+                    break;
+            }
+        }
 
-		// clear the window with a black color
-		window.clear(sf::Color::Black);
+        // clear the window with a black color
+        window.clear(sf::Color::Black);
 
-		// draw all of the parts of the game
-		board->displayBoard(window);
-		pieces->displayPieces(window);
+        // draw all of the parts of the game
+        board->displayBoard(window);
+        pieces->displayPieces(window);
 
-		// display the window contents on screen
-		window.display();
-	}
+        // display the window contents on screen
+        window.display();
+    }
 
-	return 0;
+    return 0;
 }
--- src/user_input/UserInput.cpp
+++ src/user_input/UserInput.cpp
@@ -1,68 +1,70 @@
 #include "UserInput.hpp"
 
-void UserInput::handleLeftClick(const sf::Vector2i& mousePosition, Pieces& pieces)
+void UserInput::handleLeftClick(const sf::Vector2i &mousePosition, Pieces &pieces)
 {
-	int col{};
-	int row{};
+    int col{};
+    int row{};
 
-	// saves the column that the mouse clicks to a variable column
-	for (int i{ 1 }; i <= 8; i++)
-	{
-		if (mousePosition.x <= 64 * i)
-		{
-			col = --i;
-			break;
-		}
-	}
+    // saves the column that the mouse clicks to a variable column
+    for (int i{1}; i <= 8; i++)
+    {
+        if (mousePosition.x <= 64 * i)
+        {
+            col = --i;
+            break;
+        }
+    }
 
-	// saves the row that the mouse clicks to a variable row
-	for (int i{ 1 }; i <= 8; i++)
-		if (mousePosition.y <= 64 * i)
-		{
-			row = --i;
-			break;
-		}
+    // saves the row that the mouse clicks to a variable row
+    for (int i{1}; i <= 8; i++)
+        if (mousePosition.y <= 64 * i)
+        {
+            row = --i;
+            break;
+        }
 
-	if (moves.first == -1)
-	{
-		moves.first = Pieces::toInt(col, row);
-		possibleMoves = pieces.getPossibleMoves(Pieces::toInt(col, row));
-		if (possibleMoves.size() == 0)
-		{
-			moves = { -1, -1 };
-		}
-	}
-	else
-	{
-		moves.second = Pieces::toInt(col, row);
-		auto output = std::find_if(possibleMoves.begin(), possibleMoves.end(), [this](const std::pair<int, std::vector<int>>& pair) {
-			return pair.first == this->moves.second;
-			});
-		if (output != possibleMoves.end())
-		{
-			if (output->second.size() == 2)
-			{
-				pieces.movePiece(moves.first, moves.second, std::vector<int>{output->second[0], output->second[1]});
-			}
-			else if (output->second.size() == 1)
-			{
-				pieces.movePiece(moves.first, moves.second, std::vector<int>{output->second[0]});
-			}
-			else
-			{
-				pieces.movePiece(moves.first, moves.second, std::vector<int>{});
-			}
-		}
-		moves = { -1, -1 };
-	}
+    if (moves.first == -1)
+    {
+        moves.first   = Pieces::toInt(col, row);
+        possibleMoves = pieces.getPossibleMoves(Pieces::toInt(col, row));
+        if (possibleMoves.size() == 0)
+        {
+            moves = {-1, -1};
+        }
+    }
+    else
+    {
+        moves.second = Pieces::toInt(col, row);
+        auto output  = std::find_if(possibleMoves.begin(), possibleMoves.end(), [this](const std::pair<int, std::vector<int>> &pair) { return pair.first == this->moves.second; });
+        if (output != possibleMoves.end())
+        {
+            if (output->second.size() == 2)
+            {
+                std::vector<int> tempVec = {output->second[0], output->second[1]};
+                pieces.movePiece(moves.first, moves.second, tempVec);
+            }
+            else if (output->second.size() == 1)
+            {
+                std::vector<int> tempVec = {output->second[0]};
+                pieces.movePiece(moves.first, moves.second, tempVec);
+            }
+            else
+            {
+                std::vector<int> tempVec;
+                pieces.movePiece(moves.first, moves.second, tempVec);
+            }
+        }
+        moves = {-1, -1};
+    }
 }
 
 // need to change the pieces to be the correct size
-// also need to make sure that the window will change to always be the correct shape (square for now)
-void UserInput::handleWindowResize(sf::RenderWindow& window, Board& board, Pieces& pieces)
+// also need to make sure that the window will change to always be the correct
+// shape (square for now)
+void UserInput::handleWindowResize(sf::RenderWindow &window, Board &board, Pieces &pieces)
 {
-	std::cout << window.getSize().x << std::endl;
-	int newSize = std::min(window.getSize().x, window.getSize().y);
-	board.setSize(newSize);
-	pieces.resize(newSize / 8.f);
+    std::cout << window.getSize().x << std::endl;
+    int newSize = std::min(window.getSize().x, window.getSize().y);
+    board.setSize(newSize);
+    pieces.resize(newSize / 8.f);
 }
